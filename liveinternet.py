c2296abe93e68d66ecd93431c7d57fbbbbecd026
Denis Serdyuk <diserdyuk@gmail.com>
2020-11-06 10:39:38 +0200

    Get text form page

--- liveinternet.py
+++ liveinternet.py
@@ -10,13 +10,16 @@ def get_text(url):    # request on page, get text, not html
 
 def write_csv(d):    # write data to csv file
     with open('liveinternet.csv', 'a') as f:
-        write = csv.writer(f)
-        pass
-
+        order = []
+        write = csv.DictWriter(f, fieldnames=order)
+        write.writerow(d)
 
 
 def main():
-    pass
+    url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page=2'
+    text_page = get_text(url)    # get text web page 
+    text = text_page.strip().split('\n')[1:]    # delite all non use symbols, split string and non use 1st elem.list
+    print(text) 
 
 
 
